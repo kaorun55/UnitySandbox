f082ea4f43b77b8c74d2facf84bde6a796723524
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-06-12 16:32:44 +0900

    UnityでWebカメラを使うサンプル

---
+++ WebCamTexture/Assets/WebCamBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿// Thank you for @nakamura001
+// http://d.hatena.ne.jp/nakamura001/20120107/1325922070
+using UnityEngine;
+using System.Collections;
+
+public class WebCamBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var devices = WebCamTexture.devices;
+        if ( devices.Length > 0 ) {
+            var webcamTexture = new WebCamTexture( 320, 240, 12 );
+            renderer.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }
+        else {
+            Debug.LogError( "Webカメラが検出できませんでした。" );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
