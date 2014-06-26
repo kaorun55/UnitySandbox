adf0eae4b3d4906b3c597eb3b140c2b5c90851e8
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-06-26 15:26:28 +0900

    PlayerPrefsを追加

---
+++ PlayerPrefs/Assets/GUIBehaviourScript1.cs
@@ -0,0 +1,67 @@
+﻿// http://docs.unity3d.com/ScriptReference/PlayerPrefs.html
+using UnityEngine;
+using System.Collections;
+
+public class GUIBehaviourScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Toggle = PlayerPrefs.GetInt( "Toggle1" ) == 0 ? false : true;
+        Text = PlayerPrefs.GetString( "Text1" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    #region Toggle
+    public bool _Toggle;
+    public bool Toggle
+    {
+        get
+        {
+            return _Toggle;
+        }
+
+        set
+        {
+            if ( _Toggle == value ) {
+                return;
+            }
+
+            _Toggle = value;
+            PlayerPrefs.SetInt( "Toggle1", value ? 1 : 0 );
+            //PlayerPrefs.Save();
+        }
+    }
+    #endregion
+
+    #region Text
+    public string _Text = "";
+    public string Text
+    {
+        get
+        {
+            return _Text;
+        }
+
+        set
+        {
+            if ( _Text == value ) {
+                return;
+            }
+
+            _Text = value;
+            PlayerPrefs.SetString( "Text1", value );
+            //PlayerPrefs.Save();
+        }
+    }
+    #endregion
+
+    void OnGUI()
+    {
+        Toggle = GUI.Toggle( new Rect( 100, 100, 100, 30 ), Toggle, "bool値" );
+        Text = GUI.TextArea( new Rect( 100, 140, 100, 30 ), Text, 10 );
+    }
+}
---
+++ PlayerPrefs/Assets/GUIBehaviourScript2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBehaviourScript2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Toggle = PlayerPrefs.GetInt( "Toggle2" ) == 0 ? false : true;
+        Text = PlayerPrefs.GetString( "Text2" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool Toggle = false;
+    public string Text = "";
+    //public string Text = PlayerPrefs.GetString( "Text" ); // これはエラー
+
+    void OnGUI()
+    {
+        var toggle = GUI.Toggle( new Rect( 250, 100, 100, 30 ), Toggle, "bool値" );
+        if ( toggle != Toggle ) {
+            Toggle = toggle;
+            PlayerPrefs.SetInt( "Toggle2", Toggle ? 1 : 0 );
+            //PlayerPrefs.Save();
+        }
+
+        var text = GUI.TextArea( new Rect( 250, 140, 100, 30 ), Text, 10 );
+        if ( text != Text ) {
+            Text = text;
+            PlayerPrefs.SetString( "Text2", Text );
+            //PlayerPrefs.Save();
+        }
+    }
+}
