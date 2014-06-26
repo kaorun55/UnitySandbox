cb7e33e0de4a68eb4688267b57edeeb32b1be618
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-06-26 14:16:46 +0900

    WebCamのサンプルを修正

--- WebCamTexture/Assets/WebCamBehaviourScript.cs
+++ WebCamTexture/Assets/WebCamBehaviourScript.cs
@@ -5,15 +5,35 @@ using System.Collections;
 
 public class WebCamBehaviourScript : MonoBehaviour
 {
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+    public bool Mirror = false;
+
+    public bool RightUp = false;
+    public bool LeftUp = false;
 
     // Use this for initialization
     void Start()
     {
         var devices = WebCamTexture.devices;
         if ( devices.Length > 0 ) {
-            var webcamTexture = new WebCamTexture( 320, 240, 12 );
+            var webcamTexture = new WebCamTexture( Width, Height, FPS );
             renderer.material.mainTexture = webcamTexture;
             webcamTexture.Play();
+
+            if( Mirror){
+                transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+            }
+
+            if ( RightUp ) {
+                var euler = transform.localRotation.eulerAngles;
+                transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z + 90 );
+            }
+            else if ( LeftUp ) {
+                var euler = transform.localRotation.eulerAngles;
+                transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z - 90 );
+            }
         }
         else {
             Debug.LogError( "Webカメラが検出できませんでした。" );
