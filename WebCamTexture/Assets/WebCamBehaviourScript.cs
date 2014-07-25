12c7701ec184f603aa97186c2cb7f6f2ea404d0b
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-07-25 16:17:30 +0900

    いろいろ

--- WebCamTexture/Assets/WebCamBehaviourScript.cs
+++ WebCamTexture/Assets/WebCamBehaviourScript.cs
@@ -8,6 +8,7 @@ public class WebCamBehaviourScript : MonoBehaviour
     public int Width = 1920;
     public int Height = 1080;
     public int FPS = 30;
+
     public bool Mirror = false;
 
     public bool RightUp = false;
@@ -17,26 +18,29 @@ public class WebCamBehaviourScript : MonoBehaviour
     void Start()
     {
         var devices = WebCamTexture.devices;
-        if ( devices.Length > 0 ) {
-            var webcamTexture = new WebCamTexture( Width, Height, FPS );
-            renderer.material.mainTexture = webcamTexture;
-            webcamTexture.Play();
-
-            if( Mirror){
-                transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-            }
-
-            if ( RightUp ) {
-                var euler = transform.localRotation.eulerAngles;
-                transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z + 90 );
-            }
-            else if ( LeftUp ) {
-                var euler = transform.localRotation.eulerAngles;
-                transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z - 90 );
-            }
-        }
-        else {
+        if ( devices.Length == 0 ) {
             Debug.LogError( "Webカメラが検出できませんでした。" );
+            return;
+        }
+
+        // WebCamテクスチャを作成する
+        var webcamTexture = new WebCamTexture( Width, Height, FPS );
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+
+        // ミラーリング
+        if ( Mirror ) {
+            transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+        }
+
+        // 縦にする
+        if ( RightUp ) {
+            var euler = transform.localRotation.eulerAngles;
+            transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z + 90 );
+        }
+        else if ( LeftUp ) {
+            var euler = transform.localRotation.eulerAngles;
+            transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z - 90 );
         }
     }
 
